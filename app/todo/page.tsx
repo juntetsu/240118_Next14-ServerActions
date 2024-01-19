45e380c202165da96f2206ba77e76ab47d3e36cf
jun <juntetsu.1012@gmail.com>
2024-01-19 09:59:11 +0900

    react-icons

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,6 +1,8 @@
 import { client } from "@/lib/client";
 import { revalidatePath } from "next/cache";
 import Link from "next/link";
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { FaPen } from "react-icons/fa";
 
 export type dataType = {
   contents: contentsType[];
@@ -39,7 +41,7 @@ export default async function Todo() {
         <h2 className="text-2xl font-bold">Create</h2>
         <form
           action={createTodo}
-          className="flex items-center justify-between mt-4"
+          className="flex items-center justify-between mt-4 px-6"
         >
           <input
             type="text"
@@ -62,11 +64,27 @@ export default async function Todo() {
           {data?.contents.map((item, index) => (
             <li
               key={item.id}
-              className="border-t py-4 last:border-b flex justify-between items-center"
+              className="border-t px-6 py-4 last:border-b flex justify-between items-center"
             >
               {item.title}
-              <div className="flex gap-4">
-                <Link href={`/todo/${item.id}`}>編集</Link>
+              <div className="flex items-center gap-4">
+                <Link
+                  href={`/todo/${item.id}`}
+                  className="border border-black w-[60px] h-[36px] rounded-md hover:opacity-50 flex items-center justify-center"
+                >
+                  <FaPen />
+                </Link>
+                <form
+                // action={deleteTodo}
+                >
+                  <input type="hidden" name="id" value={item.id} />
+                  <button
+                    type="submit"
+                    className="flex items-center justify-center bg-red-600 text-white w-[60px] h-[36px] rounded-md hover:opacity-50"
+                  >
+                    <BsFillTrash3Fill />
+                  </button>
+                </form>
               </div>
             </li>
           ))}
